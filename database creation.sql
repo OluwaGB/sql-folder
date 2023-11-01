b637fb58139dc814897397579cdac8872d72062b
I T_code <oluwagbotemioyeboade@gmail.com>
2023-11-01 15:31:51 -0700

    sql as a manipulation  languaage

--- database creation.sql
+++ database creation.sql
@@ -7,3 +7,6 @@ DESCRIBE memberinfo;
 INSERT INTO memberinfo(fullname,age,gender,phoneno)VALUES
 ("oyeboade olusola","N/A","Male",08030629875),("oyeboade Temitope","N/A","Male",07061042486),("oyeboade Iyanuoluwa","N/A","Male",08060025430),("oyeboade felicia","N/A","Male",07067908425);
 SELECT * FROM memberinfo;
+SELECT FROM memberinfo WHERE gender = male;
+ALTER TABLE memberinfo gender sex varchar(6);
+SELECT FROM memberinfo where swx = male;
